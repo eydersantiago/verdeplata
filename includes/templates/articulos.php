aee062db715c083221f8cf506e801526ff7f1eac
Eyder Santiago Suarez Chavez <eyder.suarez@correounivalle.edu.co>
2025-02-02 22:07:50 -0500

    mostrar articulos desde db en index and articulos-page

---
+++ includes/templates/articulos.php
@@ -0,0 +1,34 @@
+<?php
+    // Importar la conexión
+    require 'includes/config/database.php';
+    $db = conectarDB();
+
+
+
+    // Consultar en la base de datos
+    $query = "SELECT * FROM articulos LIMIT $limite";
+
+    // Obtener resultados
+    $resultado = mysqli_query($db, $query);
+?>
+
+<div class="contenedor-anuncios">
+    <?php while($articulo = mysqli_fetch_assoc($resultado)): ?>
+        <div class="anuncio">
+            <img loading="lazy" src="/imagenes/<?php echo $articulo['imagen'] ?>" alt="anuncio">
+            <div class="contenido-anuncio">
+                <h3><?php echo $articulo['titulo']?></h3>
+                <p><?php echo $articulo['descripcion']?></p>
+                <p class="precio">$<?php echo $articulo['precio']?></p>
+                <a href="articulo.php?id=<?php echo $articulo['id']; ?>" class="boton-amarillo-block">
+                    Ver Artículo
+                </a>
+            </div>
+        </div>
+    <?php endwhile; ?>
+</div>
+
+<?php
+    // Cerrar la conexión
+    mysqli_close($db);
+?>
